23f5579918ebe5f06bced4bd8db8b88b16f7b906
Ivo <ivo.iliev@vodafone.com>
2023-04-05 12:40:49 +0100

    feat: init npmrc

--- scripts/initNpmRc.js
+++ scripts/initNpmRc.js
@@ -1,1 +1,51 @@
+const path = require("path");
+const fs = require("fs");
+
 console.log("Initializing .npmrc file...");
+
+console.log("DIRECTORy", fs.readdirSync(path.join(__dirname, "..")));
+// Open LOCAL .npmrc file
+const localNpmrcPath = path.join(__dirname, "..", "local.npmrc");
+// Replace content of the file with the content of the environment variable
+fs.readFile(localNpmrcPath, "utf8", function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  // replace registry
+  let result = data.replace(/\$REGISTRY/g, process.env.NPMRC_REGISTRY);
+
+  fs.writeFile(localNpmrcPath, result, "utf8", function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+// Open ROOT .npmrc file
+const rootNpmrcPath = path.join(__dirname, "..", "root.npmrc");
+// Replace content of the file with the content of the environment variable
+fs.readFile(rootNpmrcPath, "utf8", function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  // replace $EMAIL
+  result = data.replace(/\$EMAIL/g, process.env.NPMRC_EMAIL);
+  // replace password
+  result = result.replace(/\$PASSWORD/g, process.env.NPMRC_PASSWORD);
+
+  fs.writeFile(rootNpmrcPath, result, "utf8", function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+// Move .local.npmrc to root folder
+fs.rename(
+  rootNpmrcPath,
+  path.join(__dirname, "..", "..", "..", ".npmrc"),
+  (err) => {
+    if (err) throw err;
+    console.log("Moved completed!");
+  }
+);
+
+console.log("Done initializing .npmrc file.");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import { ThemeProvider } from "styled-components";
+import themeWS10 from "@vfuk/core-theme-ws10";
+import SimpleBadge from "@vfuk/core-simple-badge";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Team 6</h1>
-    </main>
+    <ThemeProvider theme={themeWS10}>
+      <main className={styles.main}>
+        <h1>Team 6</h1>
+        <SimpleBadge />
+      </main>
+    </ThemeProvider>
   );
 }
