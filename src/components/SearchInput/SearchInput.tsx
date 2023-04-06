a0d3513c05847d8265ea5fe46f9316f697b41c3a
Ivo <ivo.iliev@vodafone.com>
2023-04-06 09:54:31 +0100

    feat: custom icon

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,7 +1,22 @@
 import React, { useEffect } from "react";
 import SearchInput from "@vfuk/core-search-input";
 
+function modifyStyles() {
+  // select a dom element by data attribute
+  const searchInput = document.querySelector(
+    '[data-custom-name="SearchInputWithIcon"]'
+  );
+  // select svg icon in searchInput
+  const svg = searchInput.querySelector(`[data-component-name="IconButton"]`);
+  // change svg icon
+  console.log("SVG: ", svg);
+  svg.innerHTML = "ask";
+}
+
 const CustomSearchInput = (props: any) => {
+  useEffect(() => {
+    modifyStyles();
+  }, []);
   return (
     <SearchInput
       textInput={{
@@ -15,6 +30,7 @@ const CustomSearchInput = (props: any) => {
         showLabel: false,
       }}
       onClear={() => props.setText("")}
+      dataAttributes={{ "data-custom-name": "SearchInputWithIcon" }}
       searchIconButton={{ onClick: props.handleAskClick }}
     />
   );
